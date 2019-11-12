5decebe1971c14dd00a746d06fe38ed893322dd1
garciacarlos78 <carles.garcia4@hotmail.com>
2019-11-12 21:16:43 +0100

    Added 2.1 Activities and intents (twoActivities)

---
+++ lesson2/README.md
@@ -0,0 +1,21 @@
+# Lesson 2 - Activities and intents
+
+Practicals related to [Android Developer Fundamentals (Version 2) - Unit 1: Get started - Lesson 2: Activities and intents](https://google-developer-training.github.io/android-developer-fundamentals-course-concepts-v2/unit-1-get-started/lesson-2-activities-and-intents/2-1-c-activities-and-intents/2-1-c-activities-and-intents.html)
+
+## 2.1 - Activities and intents (twoActivities)
+
+[Related codelab](https://codelabs.developers.google.com/codelabs/android-training-create-an-activity/#0).
+
+Create an activity that launches a second activity.
+
+### Goals
+  - Use of `parentActivityName` attribute to indicate parent-child activity relationship (*Up navigation*).
+  - Use of `meta-data` to define parent-child relationship for APIs earlier to 16.
+  - User of `Bundle` to send data between aplications.
+
+### Learned concepts
+  - `android:label` in manifest.xml adds the Activity's title to the app bar.
+  - `android:visibility="invisible"`: make initially invisible the view where applied.
+  
+  
+
---
+++ lesson2/twoActivities/src/main/java/com/cgrdev/lesson2/MainActivity.java
@@ -0,0 +1,61 @@
+package com.cgrdev.lesson2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+  // Constant for Log messages
+  private static final String LOG_TAG = MainActivity.class.getSimpleName();
+
+  // Constant for the Intent extra
+  public static final String EXTRA_MESSAGE = "com.cgrdev.lesson2.twoActivities.extra.MESSAGE";
+
+  // Constant for the response
+  public static final int TEXT_REQUEST = 1;
+
+  // Private variables to hold reply header and TextView
+  private TextView mReplyHeadTextView;
+  private TextView mReplyTextView;
+
+  private EditText mMessageEditText;
+
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+
+    mReplyHeadTextView = findViewById(R.id.text_header_reply);
+    mReplyTextView = findViewById(R.id.text_message_reply);
+
+    mMessageEditText = findViewById(R.id.editText_main);
+  }
+
+  public void launchSecondActivity(View view) {
+    Log.d(LOG_TAG, "Button clicked!");
+    Intent intent = new Intent(this, SecondActivity.class);
+    String message = mMessageEditText.getText().toString();
+    intent.putExtra(EXTRA_MESSAGE, message);
+    startActivityForResult(intent, TEXT_REQUEST);
+  }
+
+  @Override
+  public void onActivityResult(int requestCode, int resultCode,
+                               Intent data) {
+    super.onActivityResult(requestCode, resultCode, data);
+    if (requestCode == TEXT_REQUEST) {
+      if (resultCode == RESULT_OK) {
+        String reply = data.getStringExtra(SecondActivity.EXTRA_REPLY);
+        mReplyHeadTextView.setVisibility(View.VISIBLE);
+        mReplyTextView.setText(reply);
+        mReplyTextView.setVisibility(View.VISIBLE);
+      }
+    }
+  }
+}
