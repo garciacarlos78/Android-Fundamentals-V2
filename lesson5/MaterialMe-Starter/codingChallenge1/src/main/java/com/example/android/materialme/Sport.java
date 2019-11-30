46f174e75f76f8c61e231e907a7a1f4112d8e279
garciacarlos78 <carles.garcia4@hotmail.com>
2019-11-30 23:23:58 +0100

    Added 5.2.2 (MaterialMe-Starter/codingChallenge1)

---
+++ lesson5/MaterialMe-Starter/codingChallenge1/src/main/java/com/example/android/materialme/Sport.java
@@ -0,0 +1,109 @@
+/*
+ * Copyright (C) 2018 Google Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.android.materialme;
+
+import android.os.Parcel;
+import android.os.Parcelable;
+
+/**
+ * Data model for each row of the RecyclerView
+ */
+class Sport implements Parcelable {
+
+    // Member variables representing the title and information about the sport.
+    private String title;
+    private String info;
+    private final int imageResource;
+    private String description;
+
+    /**
+     * Constructor for the Sport data model.
+     *
+     * @param title         The name if the sport.
+     * @param info          Information about the sport.
+     * @param imageResource
+     */
+    Sport(String title, String info, int imageResource, String description) {
+        this.title = title;
+        this.info = info;
+        this.imageResource = imageResource;
+        this.description = description;
+    }
+
+    protected Sport(Parcel in) {
+        title = in.readString();
+        info = in.readString();
+        imageResource = in.readInt();
+        description = in.readString();
+    }
+
+    public static final Creator<Sport> CREATOR = new Creator<Sport>() {
+        @Override
+        public Sport createFromParcel(Parcel in) {
+            return new Sport(in);
+        }
+
+        @Override
+        public Sport[] newArray(int size) {
+            return new Sport[size];
+        }
+    };
+
+    /**
+     * Gets the title of the sport.
+     *
+     * @return The title of the sport.
+     */
+    String getTitle() {
+        return title;
+    }
+
+    /**
+     * Gets the info about the sport.
+     *
+     * @return The info about the sport.
+     */
+    String getInfo() {
+        return info;
+    }
+
+    public int getImageResource() {
+        return imageResource;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(title);
+        parcel.writeString(info);
+        parcel.writeInt(imageResource);
+        parcel.writeString(description);
+    }
+
+    @Override
+    public String toString() {
+        return title;
+    }
+}
